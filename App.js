ae6aa8a61448191a64147637709db6959e3866d9
Lucia Sheridan <nluciasheridan@gmail.com>
2022-04-07 15:57:28 -0300

    Automatic Context Creation

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
+
 const navigator = createStackNavigator({
   Index: IndexScreen,
 },{
@@ -10,14 +11,15 @@ const navigator = createStackNavigator({
   initialNavigationOptions: {
     title: 'Blogs'
   }
-});
+}
+);
 
 const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext  = React.createContext();
-
-export const BlogProvider = ({children} ) => {
-  const [blogPost, setBlogPost] = useState([]);
-
-  const addBlogPost = () => { 
-    setBlogPost([ ...blogPost, { title: `Blog Post #${blogPost.length + 1}` }]);
+const blogReducer = (state, action) => {
+  switch(action.type) {
+    case 'add_blogpost':
+      return [...state,  {title: `Blog Post #${state.length + 1}`}]
+    default:
+      return state;
   }
-
-  return <BlogContext.Provider value={{ data: blogPost, addBlogPost }}>
-    {children}
-  </BlogContext.Provider>
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+  return () => {
+  dispatch({ type: 'add_blogpost'})
+  };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, 
+  { addBlogPost }, 
+  []
+);
