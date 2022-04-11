c5b7262f1e1054d41540acead9af6efce0e4a023
Lucia Sheridan <nluciasheridan@gmail.com>
2022-04-11 16:56:55 -0300

    Updated BlogContext, EditScreen, CreateScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,12 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch(action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id
+        ? action.payload
+        : blogPost;
+      });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
@@ -13,8 +19,10 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
-  dispatch({ type: 'add_blogpost', payload: {title, content}});
-  callback();
+    dispatch({ type: 'add_blogpost', payload: {title, content}});
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -24,7 +32,16 @@ const deleteBlogPost = dispatch => {
   };
 }
 
+const editBlogPost = dispatch => {
+  return (id, title, content, callback ) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content}})
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(blogReducer, 
-  { addBlogPost, deleteBlogPost }, 
+  { addBlogPost, deleteBlogPost, editBlogPost }, 
   [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1}]
 );
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-  return <View>
-    <Text> Edit Screen - {navigation.getParam('id')} </Text>
-  </View>
-};
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
 
-const styles = StyleSheet.create({})
+  const blogPost = state.find(
+    blogPost => blogPost.id === id
+    );
+
+  return ( 
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+  }
+}/>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
 
 export default EditScreen;
